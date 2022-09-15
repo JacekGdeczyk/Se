8608a4516886664cbd8689fed0e090ffe2283a6d
JacekGdeczyk <jacekgdeczyk0@gmail.com>
2022-09-15 17:19:27 +0200

    Se g fb

---
+++ src/test/java/org/example/Chrome_Test.java
@@ -0,0 +1,22 @@
+package org.example;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class Chrome_Test {
+    public static void main(String[] args){
+        System.setProperty("webdriver.chrome.driver", "C://Users/hp1/.jdks/openjdk-18.0.2.1/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        driver.navigate().to("https://www.facebook.com");
+        System.out.println(driver.getTitle());
+        driver.navigate().back();
+        System.out.println(driver.getTitle());
+        driver.navigate().forward();
+        System.out.println(driver.getTitle());
+        driver.navigate().refresh();
+
+    }
+
+}
